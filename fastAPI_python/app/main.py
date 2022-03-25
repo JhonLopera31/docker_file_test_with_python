08c3b4206aca9abb1ffd625f3962e2d5a09e860d
Jhon_lopera_laptop <jhon.lopera05rep@gmail.com>
2022-03-24 19:00:29 -0500

    Updating in main.py including the neccesary modification to run de api rest in docker. Setting port and host y app.run

--- fastAPI_python/app/main.py
+++ fastAPI_python/app/main.py
@@ -15,4 +15,6 @@ def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__=="__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, port=8000,host="0.0.0.0") # To work with docker the port and host mus be spicified
+    #The to run the docker image the it is necessaty to set the port from the outside and the port of the container
+    #docker run -p 8000:000 [image name]
